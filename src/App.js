ee900d1c389529920701d020e8ac4eeb6d99ce45
w.kashiwagi <kpablow1122@gmail.com>
2025-04-08 16:29:06 +0900

    Clockコンポーネントを追加し、Appコンポーネントを更新して現在の時刻を表示するように変更

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
-
-
+import Clock from './Clock';
 
 function App() {
-  function greetingName(name) {
-    if (name) {
-      return <h1>{`Hello, ${name.firstName} ${name.lastName}`}</h1>
-    } else {
-      return <h1>Hello, Stranger</h1>
-    }
-
-  }
-
-  const name = {
-    firstName: "pani",
-    lastName: "py"
-  }
-
   return (
-    <>
-      {greetingName(name)}
-    </>
+    <Clock date={new Date()} />
   );
 }
 
