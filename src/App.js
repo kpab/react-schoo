5472e4a5e0d0b9176e74015328870793d09d8b87
w.kashiwagi <kpablow1122@gmail.com>
2025-04-08 17:47:37 +0900

    Appコンポーネントにクリックイベントハンドラーを追加し、クリック時にコンソールにメッセージを表示するように変更

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
-import Clock from './Clock';
-
 function App() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('clicked!')
+  }
   return (
-    <Clock />
+    <div>
+      <h1 onClick={handleClick}>
+        click me
+      </h1>
+    </div>
   );
 }
 
