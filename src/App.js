2fec1c0c4a3057928f7f8ee89a85e64ce65fb6c2
w.kashiwagi <kpablow1122@gmail.com>
2025-04-08 16:00:59 +0900

    名前

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+
+
 function App() {
-  const name = "poni"
+  function formatName(name) {
+    return `${name.firstName} ${name.lastName}`
+  }
+
+  const name = {
+    firstName: "pani",
+    lastName: "py"
+  }
+
   return (
-    <h1>Hello, {name}</h1>
+    <h1>Hello, {formatName(name)}</h1>
 
   );
 }
