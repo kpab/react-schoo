934e79299a4ebbfaf5b821e439c0e75abba07b45
w.kashiwagi <kpablow1122@gmail.com>
2025-04-08 16:38:47 +0900

    Clockコンポーネントにタイマー機能を追加し、1秒ごとに現在の時刻を更新するように変更

--- src/Clock.js
+++ src/Clock.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React from 'react'
 
 class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = { date: new Date() };
     }
+
+    // コンポーネントがマウントされたときに呼ばれる
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    // コンポーネントがアンマウント(削除)されるときに呼ばれる
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    // dateの書き換え
+    tick() {
+        // setStateを使ってstateを更新する
+        // setStateは非同期処理なので、stateの値を直接参照することはできない
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello, World!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-            </div >
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
         );
     }
 }
