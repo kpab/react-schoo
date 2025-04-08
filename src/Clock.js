b667a6de307770a51da1cbda4487dd45756fb880
w.kashiwagi <kpablow1122@gmail.com>
2025-04-08 16:32:09 +0900

    Clockコンポーネントを関数コンポーネントからクラスコンポーネントに変更

--- src/Clock.js
+++ src/Clock.js
@@ -1,10 +1,14 @@
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
-            <h2>It is {props.date.toLocaleTimeString()}</h2>
-        </div>
-    )
+import React from 'react';
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}</h2>
+            </div >
+        );
+    }
 }
 
 export default Clock;
