a91c4056f58eb06beca74ea8f1b3c7dcda79ca51
w.kashiwagi <kpablow1122@gmail.com>
2025-04-18 15:20:48 +0900

    handleClick関数を修正し、クリックイベントと名前をログ出力するように変更。表示内容をh1からp要素に変更。

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 function App() {
-  function handleClick() {
-    console.log('clicked!')
+  function handleClick(e, name) {
+    e.preventDefault();
+    console.log(e);
+    console.log(name);
   }
   return (
     <div>
-      <h1 onClick={handleClick}>
-        click me
-      </h1>
+      <p onClick={e => handleClick(e, 'John')}>John</p>
+      <p onClick={e => handleClick(e, 'Paul')}>Paul</p>
     </div>
   );
 }
