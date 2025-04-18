d3ba47373360962fd097079ff0d586fbe69f77dd
w.kashiwagi <kpablow1122@gmail.com>
2025-04-18 15:15:31 +0900

    ClockコンポーネントをAppコンポーネント内に統合し、内部状態で現在の時刻を表示するように修正

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import Clock from "./Clock";
+import React from 'react'
 
-function App() {
-  return (
-    <div>
-      <Clock />
-    </div>
-  );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Clock;
